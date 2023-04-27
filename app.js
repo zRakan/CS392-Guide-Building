9446a72f463627e4f88d7d852d70b6c90d8bd794
zRakan <122341529+zRakan@users.noreply.github.com>
2023-04-27 13:45:29 +0300

    ES Module / Environment configuration

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const express = require("express"),
-    path = require("path");
+import express from "express";
+import path from "path";
+import ejs from "ejs"
 
-const PORT = 3000;
+// Environment
+    import dotenv from "dotenv";
+    dotenv.config();
+
+// HTTPS
+    import https from "https";
+
+// fs
+    import fs from "fs"
+
+
 const app = express();
 
-const maintenance = true
+const maintenance = process.env.MAINTENANCE == "true"
 
-app.engine('html', require('ejs').renderFile);
-
+app.engine('html', ejs.renderFile);
 
 app.get('/', function(req, res) {
     res.render(maintenance ? "maintenance.html" : "test");
@@ -17,7 +27,22 @@ app.get("*", function(req, res) {
     res.redirect("/");
 });
 
+if(process.env.SSL_KEY != "" && process.env.SSL_CERT != "") { // Check if environment has SSL cert or not
+    
 
-app.listen(PORT, function() {
-    console.log("Started webserver [" + PORT + "]");
-})
+    // SSL
+    const httpsConnection = https.createServer({
+        key: fs.readFileSync(process.env.SSL_KEY),
+        cert: fs.readFileSync(process.env.SSL_CERT),
+    }, app)
+
+    httpsConnection.listen(443, () => {
+        console.log("Started webserver");
+    });
+} else {
+    console.log("Environment doesn't have SSL cert, switching to http...")
+
+    app.listen(80, function() {
+        console.log("Started webserver");
+    });
+}
